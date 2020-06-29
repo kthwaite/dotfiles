3acbbd1eb4069c40595870e069174c720b78594b
cthwaite <28732700+cthwaite@users.noreply.github.com>
2020-06-29 20:47:11 +0100

    feat(zsh): Add features to v.zsh, ensure backwards-compatibility

--- zsh/v.zsh
+++ zsh/v.zsh
@@ -1,16 +1,55 @@
 # tiny virtualenv manager
+# using [[ -n $VAR ]] rather than [[ -v VAR ]] here for compatibility with
+# zsh 5.0.2 (released 2012-12-21). thanks, Amazon Linux! Thamazonlinux.
 export V_VIRTUALENV_HOME="${HOME}/.virtualenvs"
 
+# 'raw' list without python versions, for zsh completion
 function v_virtualenv_list_raw {
-    find $V_VIRTUALENV_HOME -path "*/bin/python" | sed -r -e "s@${V_VIRTUALENV_HOME}/(.*)/bin/python@\1@g"| sort
+    find "$V_VIRTUALENV_HOME" -path "*/bin/python" | sed -E -e "s@${V_VIRTUALENV_HOME}/(.*)/bin/python@\1@g"| sort
 }
 
+# list virtualenvs with python versions
 function v_virtualenv_list {
-    for python in $(find $V_VIRTUALENV_HOME -path "*/bin/python" | sort); do
-        local NAME=$(echo $python | sed -r -e "s@${V_VIRTUALENV_HOME}/(.*)/bin/python@\1@g")
-        local VER=$($python -c 'import sys;print(sys.version.split()[0],end="")')
+    local NAME
+    local VER
+    for python in $(find "$V_VIRTUALENV_HOME" -path "*/bin/python" | sort); do
+        NAME=$(echo "$python" | sed -E -e "s@${V_VIRTUALENV_HOME}/(.*)/bin/python@\1@g")
+        VER=$($python -c 'import sys;print(sys.version.split()[0],end="")')
         echo "${NAME} (${VER})"
     done
+}
+
+# upgrade virtualenv to use the current python version
+function v_virtualenv_upgrade {
+    local -r V_ENV=$1
+    local -r V_DIR="$V_VIRTUALENV_HOME/$V_ENV"
+    if [ ! -e "$V_DIR" ]; then
+        echo "Virtual environment does not exist: '$V_ENV'"
+        return 127
+    fi
+    python3 -m venv --upgrade "$V_DIR"
+}
+
+# create a new virtual environment
+function v_virtualenv_new {
+    local -r V_ENV=$1
+    local -r V_DIR="$V_VIRTUALENV_HOME/$V_ENV"
+    if [ -e "$V_DIR" ]; then
+        echo "Virtual environment already exists: '$V_ENV'"
+        return 127
+    fi
+    python3 -m venv "$V_DIR" --prompt "$V_ENV"
+}
+
+function v_virtualenv_rm {
+    local -r V_ENV=$1
+    local -r V_ENV_DIR="${V_VIRTUALENV_HOME}/${V_DIR}"
+    if [ -e V_ENV_DIR ]; then
+        rm -r "$V_ENV_DIR"
+    else
+        echo "Virtual environment does not exist: '$V_ENV'"
+        return 127
+    fi
 }
 
 function v_virtualenv_switch {
@@ -20,15 +59,30 @@ function v_virtualenv_switch {
         echo "No such virtual environment: '${V_ENV}'"
         return 127
     fi
-    if [[ -v VIRTUAL_ENV ]]; then
+    if [[ -n $VIRTUAL_ENV ]]; then
         deactivate
     fi
-    source ${V_ACTIVATE}
+    source "${V_ACTIVATE}"
+}
+
+function v_virtualenv_help {
+    echo "v :: ${V_VIRTUALENV_HOME}"
+    echo
+    echo "Usage: v                  list virtualenvs OR deactivate current virtualenv"
+    echo "Usage: v <virtualenv>     activate virtualenv"
+    echo
+    echo "Usage: v <command> [<args>]"
+    echo
+    echo "      -d / --deactivate   - Deactivate the current virtualenv"
+    echo "      -h / --help         - Print this message"
+    echo "      -l / --list         - List virtualenvs"
+    echo "      -n / --new          - Create a new virtualenv"
+
 }
 
 function v {
     if [[ $# -eq 0 ]]; then
-        if [[ -v VIRTUAL_ENV ]]; then
+        if [[ -n $VIRTUAL_ENV ]]; then
             deactivate
         else
             v_virtualenv_list
@@ -37,16 +91,33 @@ function v {
     fi
 
     case "$1" in
+        -d|--deactivate)
+            if [[ -n $VIRTUAL_ENV ]]; then
+                deactivate
+            else
+                echo "No virtual environment currently active."
+            fi
+            ;;
+        -h|--help)
+            v_virtualenv_help
+            ;;
         -l|--list)
             shift
-            v_virtualenv_list $@
+            v_virtualenv_list "$@"
+            ;;
+        -n|--new)
+            shift;
+            v_virtualenv_new "$@"
+            ;;
+        -u|--upgrade)
+            shift;
+            v_virtualenv_upgrade "$@"
             ;;
         *)
-            v_virtualenv_switch $1
+            v_virtualenv_switch "$@"
             ;;
     esac
 }
-
 
 function v_install_completion {
     _v_virtualenv_list () {
