ec92ba8a9308224bf87a11569d8a39477c9d1eaf
cthwaite <28732700+cthwaite@users.noreply.github.com>
2020-08-27 10:11:36 +0100

    refactor(zsh): Rewrite pacsrc helper as macro

--- zsh/arch.zsh
+++ zsh/arch.zsh
@@ -2,8 +2,13 @@ if [[ -x "$(command -v fzf)" ]]; then
     # -- fzf package helpers
     alias pacfzf="pacman -Slq | fzf -m --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S"
     alias yayfzf="yay -Slq | fzf -m --preview 'yay -Si {1}' | xargs -ro yay -S"
-    alias pacsrc="pacman -Qeq | fzf -m --preview 'pacman -Qi {1}'"
+    function pacsrc {
+        local -r pack=$(pacman -Qei |awk '/^Name/ { name=$3 } /^Groups/ { if ( $3 != "base" && $3 != "base-devel" ) { print name } }')
+        echo $pack | fzf -m --preview 'pacman -Qi {1}'
+    }
 fi
+
+
 
 power-consumption() {
     upower -i /org/freedesktop/UPower/devices/battery_BAT0
