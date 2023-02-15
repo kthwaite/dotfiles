963e0e7bd78e57e8280ac7acfd1c79cc9616c659
kthwaite <28732700+cthwaite@users.noreply.github.com>
2023-02-15 13:52:47 +0000

    refactor,style(nvim): Lua styling, minor tweaks to load order

--- nvim/lua/plugins/neo_tree.lua
+++ nvim/lua/plugins/neo_tree.lua
@@ -1,30 +1,30 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v2.x",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-		"nvim-lua/plenary.nvim",
-	},
-	lazy = true,
-	cmd = "Neotree",
-	deactivate = function()
-		vim.cmd([[Neotree close]])
-	end,
-	init = function()
-		vim.g.neo_tree_remove_legacy_commands = 1
-		if vim.fn.argc() == 1 then
-			local stat = vim.loop.fs_stat(vim.fn.argv(0))
-			if stat and stat.type == "directory" then
-				require("neo-tree")
-			end
-		end
-	end,
-	--    { "<leader>ft", "<cmd>Neotree toggle<CR>", desc = "Neotree" },
-	opts = {
-		filesystem = {
-			follow_current_file = true,
-			hijack_netrw_behavior = "open_current",
-		},
-	},
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+    },
+    lazy = true,
+    cmd = "Neotree",
+    deactivate = function()
+        vim.cmd([[Neotree close]])
+    end,
+    init = function()
+        vim.g.neo_tree_remove_legacy_commands = 1
+        if vim.fn.argc() == 1 then
+            local stat = vim.loop.fs_stat(vim.fn.argv(0))
+            if stat and stat.type == "directory" then
+                require("neo-tree")
+            end
+        end
+    end,
+    keys = { "<leader>ft", "<cmd>Neotree toggle<CR>", desc = "Neotree" },
+    opts = {
+        filesystem = {
+            follow_current_file = true,
+            hijack_netrw_behavior = "open_current",
+        },
+    },
 }
